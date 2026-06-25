fd57246ebacbdbc9c076d9e0193c1051bc31ef1d
kamronbek.m <mardonovk233@gmail.com>
2026-06-25 11:33:59 +0500

    parser_004

--- router.py
+++ router.py
@@ -8,7 +8,7 @@ async def test():
     return 'tests'
 
 @router.post('/parser')
-async def parser(file: UploadFile = File(...)):
+async def parser(file: UploadFile = File(...), body: bool = None, titles: str = None):
     service = ParserService()
     content = await file.read()
-    return await service.parser(content, file.filename)
+    return await service.parser(content, file.filename, body, titles)
