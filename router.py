1481978734acc6d19802d67251c4688ff6d518d2
kamronbek.m <mardonovk233@gmail.com>
2026-06-25 12:27:52 +0500

    parser_005 pdf json need to upgrade

--- router.py
+++ router.py
@@ -8,7 +8,7 @@ async def test():
     return 'tests'
 
 @router.post('/parser')
-async def parser(file: UploadFile = File(...), body: bool = None, titles: str = None):
+async def parser(file: UploadFile = File(...), body: bool = True, titles: str = None):
     service = ParserService()
     content = await file.read()
     return await service.parser(content, file.filename, body, titles)
